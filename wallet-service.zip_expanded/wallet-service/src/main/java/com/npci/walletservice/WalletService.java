014433e7d661ecca8ee51d832000aa97601b92cf
Shivenrda Shakya <rajat130999@gmail.com>
2024-04-23 15:51:35 +0530

    circuit breaker

--- wallet-service.zip_expanded/wallet-service/src/main/java/com/npci/walletservice/WalletService.java
+++ wallet-service.zip_expanded/wallet-service/src/main/java/com/npci/walletservice/WalletService.java
@@ -4,6 +4,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+
 @Service
 public class WalletService {
 	@Autowired
@@ -12,7 +14,10 @@ public class WalletService {
 	@Autowired
 	private Client accountClient;
 	
+	@CircuitBreaker(name="getAccount", fallbackMethod="getAccountDetails2")
 	public Wallet getAccountDetails(long accountNumber) {
+		
+		System.out.println("_______Making Remote call_________");
 		String URL = "http://ACCOUNT-SERVICE/account/" + accountNumber;
 //		Account account = rest.getForObject(URL, Account.class);  // from restTemplt
 		
@@ -22,6 +27,12 @@ public class WalletService {
 		
 		Wallet wallet = new Wallet("MyPay", 500, (500+account.getBalance()), account);
 		
+		return wallet;
+	}
+	
+	public Wallet getAccountDetails2(long accountNumber, Throwable t) {
+		System.out.println("___________FallBack Method_________");
+		Wallet wallet = new Wallet("My pay", 500, 0 , null);
 		return wallet;
 	}
 }
